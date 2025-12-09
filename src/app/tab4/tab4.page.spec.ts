dadf28570ee8ac29359245ef9709231d73ddffd5
Jxonyendi <joseph.onyendi@gmail.com>
2025-12-08 21:57:25 -0600

    Save recent local changes (autosave branch local/saved-20251208-215725)

---
+++ src/app/tab4/tab4.page.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrderPage } from './tab4.page';
+
+describe('OrderPage', () => {
+  let component: OrderPage;
+  let fixture: ComponentFixture<OrderPage>;
+
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(OrderPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrderPage } from './tab4.page';
+
+describe('OrderPage', () => {
+  let component: OrderPage;
+  let fixture: ComponentFixture<OrderPage>;
+
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(OrderPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
